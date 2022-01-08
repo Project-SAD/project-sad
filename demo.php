3da568c92d8be6e9686f62f99b9079bbabb8c99d
Project-SAD <96688087+Project-SAD@users.noreply.github.com>
2022-01-08 12:08:50 +0100

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,14 @@
+<?php
+
+    // $mysqli = new mysqli('localhost', 'root', 'passowrd', '')
+    $dbServername = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "server";
+
+
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+    
+
+?>
